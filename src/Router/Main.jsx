87f73675e61dbbb8d9e8008387634001953c32c8
sazzad <sajusajjad052@gmail.com>
2023-06-01 09:27:38 +0600

    dark and light mode emplement

---
+++ src/Router/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../component/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Main;
